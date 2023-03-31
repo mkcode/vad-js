534bedf6dfe06dcbe2402299692c81cb782e4d73
Chris Ewald <chrisewald@gmail.com>
2023-03-30 23:14:25 -0400

    Import code. Mostly working.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export const foo = 'foo'
+
+export async function initializeRnnoise(workletUrl: string | URL, audioContext: AudioContext): Promise<AudioWorkletNode | undefined> {
+    // await audioContext.resume();
+
+    // const baseUrl = `${getBaseUrl()}libs/`;
+    // const workletUrl = `${baseUrl}noise-suppressor-worklet.min.js`;
+    // const workletUrl = `./vadNodeWorklet.js`;
+    // await audioContext.audioWorklet.addModule(workletUrl);
+
+    try {
+        await audioContext.audioWorklet.addModule(workletUrl);
+    } catch (e) {
+        console.error('Error while adding audio worklet module: ', e);
+
+        return;
+    }
+
+    // After the resolution of module loading, an AudioWorkletNode can be constructed.
+
+    return new AudioWorkletNode(audioContext, 'NoiseSuppressorWorklet');
+
+}
